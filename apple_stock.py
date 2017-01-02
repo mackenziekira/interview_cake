ac317143e20a66adc029b0b4c46ebc3c39799958
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-02 14:45:52 -0800

    wrote docstrings. practicing test driven development

---
+++ apple_stock.py
@@ -0,0 +1,19 @@
+def get_max_profit(stock_prices_yesterday):
+    """returns max profit you could have made off a list of stock prices
+
+    >>> get_max_profit([10, 7, 5, 8, 11, 9])
+    6
+
+    >>> get_max_profit([10, 7, 5, 8, 11, 9, 4])
+    6
+
+    >>> get_max_profit([10])
+    0
+
+    >>> get_max_profit([10, 10, 10, 10])
+    0
+    """
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
