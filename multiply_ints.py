65638f794ee3e9080cbeb7b3b246f9f1df8efc03
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-05 15:22:38 -0800

    set up initial doctest for second interview cake problem

---
+++ multiply_ints.py
@@ -0,0 +1,13 @@
+def multiply_ints(list_of_ints):
+    """multiplies a list of ints by all ints in list but the int at that index
+
+    >>> multiply_ints([1, 7, 3, 4])
+    [84, 12, 28, 21]
+
+    """
+
+    
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
