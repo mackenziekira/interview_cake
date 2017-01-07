843510cc99456debf4ba9cee16cb2d6da35bdaf6
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-07 14:54:49 -0800

    solution with linear space and time complexity

--- multiply_ints.py
+++ multiply_ints.py
@@ -4,23 +4,34 @@ def multiply_ints(list_of_ints):
     >>> multiply_ints([1, 7, 3, 4])
     [84, 12, 28, 21]
 
+    >>> multiply_ints([])
+    []
+
+    >>> multiply_ints([0, 10])
+    [10, 0]
+
+    >>> multiply_ints([0, 10, 1])
+    [10, 0, 0]
+
+    >>> multiply_ints([1])
+    [1]
+
     """
-    products_of_all_ints_before_ind = [None] * len(list_of_ints)
-    preproduct = 1
-    
-    for idx, num in enumerate(list_of_ints):
-        products_of_all_ints_before_ind[idx] = preproduct
-        preproduct *= num
 
-
-    products_of_all_ints_after_ind = [None] * len(list_of_ints)
+    products_of_all_ints_except_ind = [None] * len(list_of_ints)
     postproduct = 1
 
     for idx in xrange(len(list_of_ints) - 1, -1, -1):
-        products_of_all_ints_after_ind[idx] = postproduct
+        products_of_all_ints_except_ind[idx] = postproduct
         postproduct *= list_of_ints[idx]
 
-    return products_of_all_ints_after_ind
+    preproduct = 1
+    
+    for idx, num in enumerate(list_of_ints):
+        products_of_all_ints_except_ind[idx] *= preproduct
+        preproduct *= num
+
+    return products_of_all_ints_except_ind
 
 
 
