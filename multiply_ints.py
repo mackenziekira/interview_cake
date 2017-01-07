e7983f06c39331a64ba0c2e17087ce0fb14b6f2e
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-07 14:16:56 -0800

    slightly more elegant solution to second interview cake problem

--- multiply_ints.py
+++ multiply_ints.py
@@ -6,11 +6,26 @@ def multiply_ints(list_of_ints):
 
     """
 
-    totals = [1 for num in list_of_ints]
-    for ind, num in enumerate(list_of_ints):
-        for m, multiplier in enumerate(list_of_ints):
-            if m != ind:
-                totals[ind] *= multiplier 
+    # totals = [1 for num in list_of_ints]
+    # for ind, num in enumerate(list_of_ints):
+    #     for m, multiplier in enumerate(list_of_ints):
+    #         if m != ind:
+    #             totals[ind] *= multiplier 
+
+    pre = preproduct_tally(list_of_ints)
+    post = reversed(preproduct_tally(reversed(list_of_ints)))
+
+    return [x * y for x, y in zip(pre, post)]
+
+def preproduct_tally(list_of_ints):
+    """keeps a running tally of product preceeding each item in a list"""
+
+    preproduct = 1
+    totals = []
+    for num in list_of_ints:
+        totals.append(preproduct)
+        preproduct *= num
+
     return totals
 
 if __name__ == "__main__":
