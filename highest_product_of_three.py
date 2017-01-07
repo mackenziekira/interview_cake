900abaf69ce32b5bdb24c5d300081528ecf84b14
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-07 15:15:03 -0800

    set up tests for highest product of three problem

---
+++ highest_product_of_three.py
@@ -0,0 +1,17 @@
+def highest_product_of_three(list):
+    """returns highest product of three numbers in a list
+
+    >>> highest_product_of_three([1, 2, 3])
+    6
+
+    >>> highest_product_of_three([])
+    None
+
+    >>> highest_product_of_three([1, 2, 3, 4])
+    24
+    
+    """
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
