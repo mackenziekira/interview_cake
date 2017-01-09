2474413e7db721d5987e299f7a106df967f462f0
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-09 12:06:59 -0800

    set up tests for coins.py

---
+++ coins.py
@@ -0,0 +1,10 @@
+def ways_to_make_change(total, coins):
+    """outputs the number of ways to reach the total given x denominations of coins
+
+    >>> ways_to_make_change(4, [1, 2, 3])
+    4
+    """
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
