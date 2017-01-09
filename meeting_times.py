cc9130783a0ea6fc43d447bb1e63f51917db8be7
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-08 19:17:13 -0800

    wrote initial test for meeting time problem

---
+++ meeting_times.py
@@ -0,0 +1,12 @@
+def meeting_times(list_of_meeting_times):
+    """returns list of tuples of all times people are in meetings.
+
+    >>> meeting_times([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+    [(0, 1), (3, 8), (9, 12)]
+    """
+
+    
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
