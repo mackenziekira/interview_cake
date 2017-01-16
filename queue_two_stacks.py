28fa0c2c91259e6e499b3a6ea9844619fa4f4140
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-15 20:09:42 -0800

    set up initial queue class

---
+++ queue_two_stacks.py
@@ -0,0 +1,21 @@
+class Queue():
+    """implement a queue using two stacks"""
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def enqueue(self, num):
+        """add a number to the queue
+        """
+        pass
+
+    def dequeue(self):
+        """pop a number off the front of the queue
+        """
+        pass
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
