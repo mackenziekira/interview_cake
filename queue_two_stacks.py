71d651fe37462e3ca361b97b1a5da3d107d4783b
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-22 17:12:13 -0800

    started solution for queue two stacks

--- queue_two_stacks.py
+++ queue_two_stacks.py
@@ -1,21 +1,41 @@
-class Queue():
-    """implement a queue using two stacks"""
+class Stack(object):
+    """a stack class"""
     def __init__(self):
-        self._queue = []
+        self.stack = []
 
     def __repr__(self):
-        return str(self._queue)
+        return str(self.stack)
+
+    def push(self, num):
+        self.stack.append(num)
+
+    def pop(self):
+        return self.stack.pop()
+
+class Queue(object):
+    """implement a queue using two stacks"""
+    def __init__(self):
+        self._instack = Stack()
+        self._outstack = Stack()
+
+    def __repr__(self):
+        return str(self._instack) + str(self._outstack)
 
     def enqueue(self, num):
         """add a number to the queue
         """
-        self._queue.append(num)
+        self._instack.push(num)
 
     def dequeue(self):
         """pop a number off the front of the queue
         """
-        first_item = self._queue[0]
-        self._queue.remove(first_item)
+        if self._outstack != []:
+            return self._outstack.pop()
+
+        while self._instack:
+            self._outstack.push(self._instack.pop())
+
+        return self._outstack.pop()
 
 
 if __name__ == "__main__":
