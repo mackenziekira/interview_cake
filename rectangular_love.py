553fb571c5b26ccf6f865e5f5fcc6e741187c4d4
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-02-01 12:16:01 -0800

    added more doctests to test for edge cases. (no intersection, one rectangle entirely contained in the other)

--- rectangular_love.py
+++ rectangular_love.py
@@ -2,6 +2,13 @@ def rectangular_love(rect1, rect2):
     """
     >>> rectangular_love( {'left_x': 1, 'bottom_y': 5, 'width': 10, 'height': 4}, {'left_x': 0, 'bottom_y': 0, 'width': 2, 'height': 6}) == {'left_x': 1, 'bottom_y': 5, 'width': 1, 'height': 1}
     True
+
+    >>> rectangular_love( {'left_x': 0, 'bottom_y': 0, 'width': 1, 'height': 1}, {'left_x': 5, 'bottom_y': 5, 'width': 2, 'height': 6}) == {'left_x': None, 'bottom_y': None, 'width': None, 'height': None}
+    True
+
+    >>> rectangular_love( {'left_x': 0, 'bottom_y': 0, 'width': 10, 'height': 10}, {'left_x': 2, 'bottom_y': 2, 'width': 2, 'height': 3}) == {'left_x': 2, 'bottom_y': 2, 'width': 2, 'height': 3}
+    True
+
     """
     result = {}
 
