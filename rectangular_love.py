b615faaaf0f775445386e7b0635dc785ead2312b
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-02-01 11:20:37 -0800

    set up doctest for function

---
+++ rectangular_love.py
@@ -0,0 +1,10 @@
+def rectangular_love(rect1, rect2):
+    """
+    >>> rectangular_love( {'left_x': 1, 'bottom_y': 5, 'width': 10, 'height': 4}, {'left_x': 0, 'bottom_y': 0, 'width': 2, 'height': 6})
+    {'left_x': 1, 'bottom_y': 5, 'width': 1, 'height': 1}
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
